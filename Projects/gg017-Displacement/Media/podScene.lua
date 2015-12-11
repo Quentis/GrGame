53973a2298f4dba99e41741c75428dd9e7a72da0
Tamas Somhegyi <somhegyi.tamas@gmail.com>
2015-12-11 19:29:54 +0100

    gg017-Displacement is added

---
+++ Projects/gg017-Displacement/Media/podScene.lua
@@ -0,0 +1,119 @@
+O:FirstPersonCam(_, {name='default', position={x=20, y=20, z=20} } )
+
+O:IndexedMesh(_, {name='giraffe', file='giraffe.obj'})
+O:Material(_, {name='spotted', technique='textured', pass='textured'}, function(_)
+	O:setSrv(_, {effectVariable='kdTexture', file='giraffe.jpg'})
+end )
+	
+O:MultiMesh(_, {name='giraffe'}, function(_)
+	O:FlipMesh(_, {}, function(_)
+		O:ShadedMesh(_, {mien='basic', indexedMesh='giraffe', material='spotted'})
+	end )
+end )
+
+O:StaticEntity(_, {name='giraffe0', multiMesh='giraffe', position = { x=2, y=0, z=0} } )
+
+
+
+O:MultiMeshFromFile(_, {name='pod', file='geopod.x'})
+
+O:PhysicsMaterial(_, {name = 'default'})
+
+for i=0,10,1 do
+	O:PhysicsEntity(_, {name='pod'..i, multiMesh='pod', position={x=0, y=20, z=i*10} }, function (_)
+		O:Dynamics(_, {linearVelocity={y=10}, angularVelocity={y=2}} )
+		O:Shape(_, {geometryType='eCAPSULE', material='default', orientationAngle=0, orientationAxis={x=0, y=1, z=0}, halfHeight=5, radius=3, position = { y=0 } } )
+		O:Shape(_, {geometryType='eBOX',     material='default', orientationAngle=0.79, orientationAxis={x=0, y=1, z=0}, halfExtents = {x=4, y=1, z=4}, position = { x=4.5, y=-1, z=0 } } )
+		O:Shape(_, {geometryType='eBOX',     material='default', orientationAngle=0.79, orientationAxis={x=0, y=1, z=0}, halfExtents = {x=4, y=1, z=4}, position = { x=-5, y=-1, z=-2 } } )
+		O:Shape(_, {geometryType='eBOX',     material='default', orientationAngle=0.79, orientationAxis={x=0, y=1, z=0}, halfExtents = {x=4, y=1, z=4}, position = { x=-5, y=-1, z=2 } } )
+		O:Shape(_, {geometryType='eBOX',     material='default', orientationAngle=1.098, orientationAxis={x=-0.146, y=-0.353, z=-0.353}, halfExtents = {x=4, y=1, z=4}, position = { x=-7, y=2, z=0 } } )
+	end )
+end
+
+
+O:MultiMeshFromFile(_, {name='ground', file='ground.x'})
+O:PhysicsEntity(_, {name='ground', multiMesh='ground', position={x=0, y=-20, z=0} }, function(_)
+	O:Shape(_, {
+			geometryType='eBOX',
+			material='default',
+			orientationAngle=0,
+			orientationAxis={x=0, y=1, z=0},
+			halfExtents = {x=1000, y=1, z=1000},
+			position = { y=0 } } )
+end )
+
+
+O:IndexedMesh(_, {name='background', file='quad.x'})
+O:Material(_, {name='skybackground', technique='envmapped', pass='background'}, function(_)
+	O:setSrv(_, {effectVariable='envTexture', file='cloudynoon.dds'})
+end )
+	
+O:MultiMesh(_, {name='skybackground'}, function(_)
+	O:FlipMesh(_, {}, function(_)
+		O:ShadedMesh(_, {mien='basic', indexedMesh='background', material='skybackground'})
+	end )
+end )
+
+O:StaticEntity(_, {name='sky', multiMesh='skybackground'} )
+
+
+
+
+O:Material(_, {name='window', technique='envmapped', pass='envmapped'}, function(_)
+	O:setSrv(_, {effectVariable='kdTexture', file='giraffe.jpg'})
+	O:setSrv(_, {effectVariable='envTexture', file='cloudynoon.dds'})
+end )
+	
+O:MultiMesh(_, {name='multipod'}, function(_)
+	O:FlipMesh(_, {}, function(_)
+		O:ShadedMesh(_, {mien='basic', indexedMesh='pod[0]', material='spotted'})
+	end )
+	O:FlipMesh(_, {}, function(_)
+		O:ShadedMesh(_, {mien='basic', indexedMesh='pod[1]', material='window'})
+	end )
+end )
+
+O:StaticEntity(_, {name='multipod0', multiMesh='multipod', position = { x=20, y=20, z=20} } )
+
+O:IndexedMesh(_, {name='rock', file='rock.obj'})
+O:Material(_, {name='normalMappedRock', technique='displacement', pass='normalMapped'}, function(_)
+  O:setSrv(_, {effectVariable='kdTexture', file='rkd.jpg'})
+  O:setSrv(_, {effectVariable='bumpTexture', file='rbump.jpg'})
+  O:setSrv(_, {effectVariable='normalTexture', file='rnormal.jpg'})
+end )
+O:MultiMesh(_, {name='normalMappedRock'}, function(_)
+  O:FlipMesh(_, {}, function(_)
+    O:ShadedMesh(_, {mien='basic', 
+             indexedMesh='rock', material='normalMappedRock'})
+  end )
+end )
+
+
+O:StaticEntity(_, {name='rock0', multiMesh='normalMappedRock', position = { x=0, y=100, z=0} } )
+
+O:Material(_, {name='parallaxMapped', technique='displacement', pass='parallaxMapped'}, function(_)
+  O:setSrv(_, {effectVariable='kdTexture', file='rkd.jpg'})
+  O:setSrv(_, {effectVariable='bumpTexture', file='rbump.jpg'})
+  O:setSrv(_, {effectVariable='normalTexture', file='rnormal.jpg'})
+end )
+O:MultiMesh(_, {name='parallaxMappedRock'}, function(_)
+  O:FlipMesh(_, {}, function(_)
+    O:ShadedMesh(_, {mien='basic',
+            indexedMesh='rock', material='parallaxMapped'})
+  end )
+end )
+O:StaticEntity(_, {name='rock1', multiMesh='parallaxMappedRock', position = { x=50, y=100, z=0} } )
+
+
+O:Material(_, {name='reliefMapped', technique='displacement', pass='reliefMapped'}, function(_)
+  O:setSrv(_, {effectVariable='kdTexture', file='rkd.jpg'})
+  O:setSrv(_, {effectVariable='bumpTexture', file='rbump.jpg'})
+  O:setSrv(_, {effectVariable='normalTexture', file='rnormal.jpg'})
+end )
+O:MultiMesh(_, {name='reliefMappedRock'}, function(_)
+  O:FlipMesh(_, {}, function(_)
+  O:ShadedMesh(_, {mien='basic', 
+           indexedMesh='rock', material='reliefMapped'})
+  end )
+end )
+O:StaticEntity(_, {name='rock2', multiMesh='reliefMappedRock', position = { x=100, y=100, z=0} } )
---
+++ fx/displacement.fx
@@ -0,0 +1,77 @@
+
+Texture2D normalTexture;
+Texture2D bumpTexture;
+
+float4 psNormalMapped(VsosTrafo input) : SV_TARGET {
+	float3 normal = normalize(input.normal);
+
+	float2 dtdx = ddx(input.tex);
+	float2 dtdy = ddy(input.tex);
+	float3 dpdx = ddx(input.worldPos);
+	float3 dpdy = ddy(input.worldPos);
+
+	float3 sTangent = (dpdx * dtdy.y - dpdy * dtdx.y);
+	float3 sBinormal = (dpdy * dtdx.x - dpdx * dtdy.x) ;
+	float3 N = normalize(cross(sBinormal, sTangent));
+	float3 B = normalize(cross(input.normal, sTangent));
+	float3 T = normalize(cross(B, input.normal));
+	//return abs(N.xyzz); 
+	float3 tangentNormal = normalize(normalTexture.Sample(linearSampler, input.tex).xzy - float3(0.5, 0.5, 0.5));
+	float3 worldNormal = B * tangentNormal.z + normal * tangentNormal.y + T * tangentNormal.x;
+	return abs(worldNormal.y) * kdTexture.Sample(linearSampler, input.tex);
+}
+
+float4 psParallaxMapped(VsosTrafo input) : SV_TARGET{
+	float3 normal = normalize(input.normal);
+
+	float2 dtdx = ddx(input.tex);
+	float2 dtdy = ddy(input.tex);
+	float3 dpdx = ddx(input.worldPos);
+	float3 dpdy = ddy(input.worldPos);
+
+	float3 sTangent = (dpdx * dtdy.y - dpdy * dtdx.y);
+	float3 sBinormal = (dpdy * dtdx.x - dpdx * dtdy.x);
+	float3 N = normalize(cross(sBinormal, sTangent));
+	float3 B = normalize(cross(input.normal, sTangent));
+	float3 T = normalize(cross(B, input.normal));
+	
+	float bump_height = 0.1;
+	float bump = bumpTexture.Sample(linearSampler, input.tex);
+	float3 viewDir = normalize(eyePos - input.worldPos);
+		viewDir = float3(dot(viewDir, T), dot(viewDir, B), dot(viewDir, normal));
+	float2 texOffset = bump_height * viewDir.xy
+		// / viewDir.z 
+		* bump;
+	input.tex += texOffset;
+
+	float3 tangentNormal = normalize(normalTexture.Sample(linearSampler, input.tex).xzy - float3(0.5, 0.5, 0.5));
+
+	float3 worldNormal = B * tangentNormal.z + normal * tangentNormal.y + T * tangentNormal.x;
+
+	return abs(worldNormal.y) * kdTexture.Sample(linearSampler, input.tex);
+}
+
+
+
+technique11 displacement
+{
+  pass normalMapped
+  {
+        SetVertexShader ( CompileShader( vs_5_0, vsTrafo() ) );
+        SetGeometryShader( NULL );
+        SetRasterizerState( defaultRasterizer );
+        SetPixelShader( CompileShader( ps_5_0, psNormalMapped() ) );
+        SetDepthStencilState( defaultCompositor, 0 );
+        SetBlendState( defaultBlender, float4( 0.0f, 0.0f, 0.0f, 0.0f ), 0xFFFFFFFF  );
+  }
+  pass parallaxMapped
+  {
+	  SetVertexShader(CompileShader(vs_5_0, vsTrafo()));
+	  SetGeometryShader(NULL);
+	  SetRasterizerState(defaultRasterizer);
+	  SetPixelShader(CompileShader(ps_5_0, psParallaxMapped()));
+	  SetDepthStencilState(defaultCompositor, 0);
+	  SetBlendState(defaultBlender, float4(0.0f, 0.0f, 0.0f, 0.0f), 0xFFFFFFFF);
+  }
+
+}
